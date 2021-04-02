898744a63897deeda3b089c1fa9ba6e5592a8e0a
Marthen Nodado <mdrnodado@gmail.com>
2021-04-02 16:28:42 -0700

    deleted Pods directory. will only be used locally

--- Pods/Target Support Files/Pods-phrasy-phrasyUITests/Pods-phrasy-phrasyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_phrasy_phrasyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_phrasy_phrasyUITests
-@end
--- Pods/Target Support Files/Pods-phrasy/Pods-phrasy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_phrasy : NSObject
-@end
-@implementation PodsDummy_Pods_phrasy
-@end
